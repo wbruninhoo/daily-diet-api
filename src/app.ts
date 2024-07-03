1a86d6b26de18dbbad86bad28ed1e165c8796f1a
Bruno Mendes <wbruninhoo@gmail.com>
2024-07-03 20:36:00 -0300

    Desafio referente ao módulo: Criando APIs RESTfull com Node.js

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify'
+
+import cookie from '@fastify/cookie'
+
+import { mealsRoutes } from './routes/meals.routes'
+import { usersRoutes } from './routes/users.routes'
+
+export const app = fastify()
+
+app.register(cookie)
+
+app.register(usersRoutes, { prefix: 'users' })
+app.register(mealsRoutes, { prefix: 'meals' })
